0fc0dc595e6dbe09f22437d14f5acd241de5a034
TJ Stankus <tj@stank.us>
2014-02-04 10:47:23 -0500

    add example for primitive obsession

---
+++ examples/primitive_obsession.rb
@@ -0,0 +1,26 @@
+require 'sinatra/base'
+
+class AGSite < Sinatra::Base
+  before do
+    @domain_data = SiteAttributes.new(request.host).get
+  end
+
+  get('/') do
+    if @domain_data[:site_type] == 'property'
+      slim 'property_home'
+    elsif @domain_data[:site_type] == 'management'
+      slim 'management_home'
+    end
+  end
+end
+
+# Imagine how the conditional in the root route can (and will) be duplicated all
+# over the place.
+#
+# This is because we've missed the abstraction. We need a PropertySite and a
+# ManagementSite.
+#
+# The implementation could go in a number of different directions, middleware, a
+# router, etc. The main thing is that once we have identified and implemented
+# the abstraction, our app/controller is the correct kind of thing, and we are
+# freed from the persistent cognitive overload of using primitives.
