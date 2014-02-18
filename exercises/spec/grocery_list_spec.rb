3c69d8b3a5a24f1192271d61b71a31628b525209
TJ Stankus <tj@stank.us>
2014-02-18 12:35:44 -0500

    Cherry pick updated spec

--- exercises/spec/grocery_list_spec.rb
+++ exercises/spec/grocery_list_spec.rb
@@ -3,11 +3,23 @@ require_relative '../grocery_list'
 
 describe GroceryList do
 
-  context 'when initialized with nil' do
-    it 'adds a new item' do
+  describe "#add" do
+    it "adds to an empty list" do
+      groceries = GroceryList.new([])
+      groceries.add('milk')
+      expect(groceries.items).to eq(['milk'])
+    end
+
+    it "adds to a nil list" do
       groceries = GroceryList.new(nil)
       groceries.add('milk')
-      expect(groceries.items).to include('milk')
+      expect(groceries.items).to eq(['milk'])
+    end
+
+    it "adds to a predefined list" do
+      groceries = GroceryList.new(['butter'])
+      groceries.add('milk')
+      expect(groceries.items).to eq(['butter','milk'])
     end
   end
 
