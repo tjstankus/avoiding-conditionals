c0f13d040c79a22af4c3f14b98b0473507748005
TJ Stankus <tj@stank.us>
2014-02-18 11:20:25 -0500

    Starting spec for exercise 1

--- exercises/spec/grocery_list_spec.rb
+++ exercises/spec/grocery_list_spec.rb
@@ -3,28 +3,50 @@ require_relative '../grocery_list'
 
 describe GroceryList do
 
+  let(:milk) { double('item', name: 'milk', price: 399, to_s: "milk: $3.99") }
+  let(:bread) { double('item', name: 'bread', price: 199, to_s: "bread: $1.99") }
+
   context 'when initialized with nil' do
     it 'adds a new item' do
       groceries = GroceryList.new(nil)
-      item = double('item', name: 'milk')
-      groceries.add(item)
-      expect(groceries.items).to include(item)
+      groceries.add(milk)
+      expect(groceries.items).to include(milk)
     end
   end
 
-  it 'calculates total cost' do
-    milk = double('item', name: 'milk', price: 399)
-    bread = double('item', name: 'milk', price: 199)
-    groceries = GroceryList.new([milk, bread])
-    expect(groceries.total_cost).to eq(5.98)
+  context 'given multiple items' do
+
+    before do
+      @groceries = GroceryList.new([milk, bread])
+    end
+
+    it 'calculates total cost' do
+      pending 'Implement without conditionals'
+      expect(@groceries.total_cost).to eq(5.98)
+    end
+
+    it 'prints a list of items' do
+      pending 'Implement without conditionals'
+      expected = "milk: $3.99\nbread: $1.99"
+      expect(@groceries.to_s).to eq(expected)
+    end
   end
 
-  context 'given a nil item' do
+  context 'given an item and nil' do
+
+    before do
+      @groceries = GroceryList.new([milk, nil])
+    end
+
     it 'calculates total cost using 0 for nils' do
-      pending 'Fix me without using a conditional nil check'
-      milk = double('item', name: 'milk', price: 399)
-      groceries = GroceryList.new([milk, nil])
-      expect(groceries.total_cost).to eq(3.99)
+      pending 'Implement without conditionals'
+      expect(@groceries.total_cost).to eq(3.99)
+    end
+
+    it 'prints a list of items' do
+      pending 'Implement without conditionals'
+      expected = "milk: $3.99\nUnknown item: $0.0"
+      expect(@groceries.to_s).to eq(expected)
     end
   end
 
